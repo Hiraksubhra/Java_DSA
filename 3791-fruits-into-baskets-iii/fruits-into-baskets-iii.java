a2aa53724be0821482baedbe6d928a2c1c735b69
Hiraksubhra <xtremewarrior05@gmail.com>
2025-08-06 23:03:39 +0530

    Time: 87 ms (48.65%) | Memory: 65.4 MB (36.94%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
@@ -1,75 +1,59 @@
 class Solution {
-
-    private int[] segTree = new int[400007];
-    private int[] baskets;
-
-    private void build(int p, int l, int r) {
+    int n;
+    int[] seg;
+    void Update(int p) { 
+        seg[p] = Math.max(seg[p << 1], seg[p << 1 | 1]); 
+    }
+    void Build(int p, int l, int r, int[] baskets) {
         if (l == r) {
-            segTree[p] = baskets[l];
+            seg[p] = baskets[l];
             return;
         }
         int mid = (l + r) >> 1;
-        build(p << 1, l, mid);
-        build((p << 1) | 1, mid + 1, r);
-        segTree[p] = Math.max(segTree[p << 1], segTree[(p << 1) | 1]);
+        Build(p << 1, l, mid, baskets);
+        Build(p << 1 | 1, mid + 1, r, baskets);
+        Update(p);
     }
-
-    private int query(int p, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l) {
-            return Integer.MIN_VALUE;
+    void Assign(int x, int v, int p, int l, int r) {
+        if (x < l || x > r) {
+            return;
         }
-        if (ql <= l && r <= qr) {
-            return segTree[p];
-        }
-        int mid = (l + r) >> 1;
-        return Math.max(
-            query(p << 1, l, mid, ql, qr),
-            query((p << 1) | 1, mid + 1, r, ql, qr)
-        );
-    }
-
-    private void update(int p, int l, int r, int pos, int val) {
         if (l == r) {
-            segTree[p] = val;
+            seg[p] = v;
             return;
         }
         int mid = (l + r) >> 1;
-        if (pos <= mid) {
-            update(p << 1, l, mid, pos, val);
-        } else {
-            update((p << 1) | 1, mid + 1, r, pos, val);
-        }
-        segTree[p] = Math.max(segTree[p << 1], segTree[(p << 1) | 1]);
+        Assign(x, v, p << 1, l, mid);
+        Assign(x, v, p << 1 | 1, mid + 1, r);
+        Update(p);
     }
-
+    int FirstLarger(int v, int p, int l, int r) {
+        if (seg[p] < v) {
+            return r + 1;
+        }
+        if (l == r) {
+            return r;
+        }
+        int mid = (l + r) >> 1;
+        int lf = FirstLarger(v, p << 1, l, mid);
+        if (lf <= mid) {
+            return lf;
+        }
+        return FirstLarger(v, p << 1 | 1, mid + 1, r);
+    }
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        this.baskets = baskets;
-        int m = baskets.length;
-        int count = 0;
-        if (m == 0) {
-            return fruits.length;
-        }
-        Arrays.fill(segTree, Integer.MIN_VALUE);
-        build(1, 0, m - 1);
-        for (int i = 0; i < fruits.length; i++) {
-            int l = 0;
-            int r = m - 1;
-            int res = -1;
-            while (l <= r) {
-                int mid = (l + r) >> 1;
-                if (query(1, 0, m - 1, 0, mid) >= fruits[i]) {
-                    res = mid;
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            if (res != -1 && baskets[res] >= fruits[i]) {
-                update(1, 0, m - 1, res, Integer.MIN_VALUE);
+        n = fruits.length;
+        seg = new int[4 * n + 1];
+        Build(1, 0, n - 1, baskets);
+        int res = 0;
+        for (int x : fruits) {
+            int pos = FirstLarger(x, 1, 0, n - 1);
+            if (pos == n) {
+                res++;
             } else {
-                count++;
+                Assign(pos, 0, 1, 0, n - 1);
             }
         }
-        return count;
+        return res;
     }
 }
