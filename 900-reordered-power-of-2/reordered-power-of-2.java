0a7807be9f9f7e76a408e18886180b73a19706dc
Hiraksubhra <xtremewarrior05@gmail.com>
2025-08-10 14:56:35 +0530

    Time: 3 ms (14.21%) | Memory: 42.4 MB (6.32%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean reorderedPowerOf2(int N) {
-        char[] a1 = String.valueOf(N).toCharArray();
-        Arrays.sort(a1);
-        String s1 = new String(a1);
-        
         for (int i = 0; i < 31; i++) {
-            char[] a2 = String.valueOf((int)(1 << i)).toCharArray();
-            Arrays.sort(a2);
-            String s2 = new String(a2);
-            if (s1.equals(s2)) return true;
+            if(counter(N).equals(counter(1<<i))){
+                return true;
+            }
         }
-        
         return false;
+    }
+    private String counter(int N){
+        int[] a = new int[11];
+        while(N>0) {
+            int d = N % 10;
+            a[d]++;
+            N/=10; 
+        }
+        return Arrays.toString(a);
     }
 }
