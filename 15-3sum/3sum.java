b41ba67cb9cb0aad37a997ae35da58b6a11f61f7
Hiraksubhra <xtremewarrior05@gmail.com>
2025-09-02 19:36:58 +0530

    Time: 31 ms (56.57%) | Memory: 51.4 MB (70.02%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i<n-2; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i+1, k = n-1;
+            while(j<k){
+                int sum = nums[j] + nums[k] + nums[i];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }else if (sum < 0) {
+            // The sum is too small, need a larger number, so move the left pointer 'j' up.
+                    j++;
+                } else { // sum > 0
+                    // The sum is too large, need a smaller number, so move the right pointer 'k' down.
+                    k--;
+                }
+            } 
+        }
+        return result;
+    }
+}
